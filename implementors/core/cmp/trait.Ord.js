01430f9df3b5bac44f816b1d0ac7dfab8312bcc0
artichoke-ci <ci@artichokeruby.org>
2020-07-11 15:24:07 +0000

    deploy: 54a8f842c0fb38051ca538da18f8f76fb6f06cc9

--- implementors/core/cmp/trait.Ord.js
+++ implementors/core/cmp/trait.Ord.js
@@ -1,4 +1,3 @@
 (function() {var implementors = {};
-implementors["bstr"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"bstr/struct.BStr.html\" title=\"struct bstr::BStr\">BStr</a>","synthetic":false,"types":["bstr::bstr::BStr"]}];
 implementors["intaglio"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"intaglio/struct.Symbol.html\" title=\"struct intaglio::Symbol\">Symbol</a>","synthetic":false,"types":["intaglio::Symbol"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++ source-files.js
@@ -1,5 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["bstr"] = {"name":"","dirs":[{"name":"byteset","files":["mod.rs","scalar.rs"]},{"name":"search","files":["byte_frequencies.rs","mod.rs","prefilter.rs","twoway.rs"]}],"files":["ascii.rs","bstr.rs","cow.rs","ext_slice.rs","impls.rs","lib.rs","utf8.rs"]};
-sourcesIndex["intaglio"] = {"name":"","files":["bytes.rs","lib.rs","str.rs"]};
-sourcesIndex["memchr"] = {"name":"","dirs":[{"name":"x86","files":["avx.rs","mod.rs","sse2.rs"]}],"files":["fallback.rs","iter.rs","lib.rs","naive.rs"]};
+sourcesIndex["intaglio"] = {"name":"","files":["bytes.rs","internal.rs","lib.rs","str.rs"]};
 createSourceSidebar();
