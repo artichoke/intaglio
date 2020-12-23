70cc8fb100a349988843de5605ab7cded5db078b
Ryan Lopopolo <rjl@hyperbo.la>
2020-12-22 19:02:51 -0800

    Exclusively use leaksan sanitizer to test for leaks
    `intaglio` includes a `libc` dev-dependency on `linux` targets for
    testing whether memory usage increases using `getrusage`. Memory usage
    outside of a tolerance is treated as a likely leak.
    
    This method of leak checking is flaky: these tests do not work on macOS
    and Windows. Additionally, nighly Rust has built in support for
    LeakSanitizer _and_ `intaglio` already runs leaksan in CI.
    
    This commit removes the `libc` dep and the associated leak detector test
    code.

--- tests/leak_drop_symbol_table.rs
+++ tests/leak_drop_symbol_table.rs
@@ -1,36 +1,26 @@
 #![deny(clippy::all)]
 #![deny(clippy::pedantic)]
 
+use core::iter;
 use intaglio::SymbolTable;
 
-mod leak;
-
-use leak::Looper;
-
 const ITERATIONS: usize = 100_000;
-const LEAK_TOLERANCE: i64 = 1024 * 1024 * 50;
 
 #[test]
 fn dealloc_owned_data_str() {
-    let table = SymbolTable::with_capacity(0);
-    Looper::new("dealloc_owned_data_str", table)
-        .with_iterations(ITERATIONS)
-        .with_tolerance(LEAK_TOLERANCE)
-        .check_leaks_with_finalizer(
-            |i, table| {
-                #[allow(clippy::cast_possible_truncation)]
-                let ch = char::from((i % 256) as u8);
-                let len = (i / 256) + 100;
-                let symbol = vec![ch; len].into_iter().collect::<String>();
-                let sym_id = table.intern(symbol.clone()).unwrap();
-                assert!(table.is_interned(&symbol));
-                assert!(table.contains(sym_id));
-                assert_eq!(Some(symbol.as_str()), table.get(sym_id));
-                assert_eq!(sym_id, table.intern(symbol.clone()).unwrap());
-                assert!(table.is_interned(&symbol));
-                assert!(table.contains(sym_id));
-                assert_eq!(Some(symbol.as_str()), table.get(sym_id));
-            },
-            drop,
-        );
+    let mut table = SymbolTable::with_capacity(0);
+    for (i, ch) in ('\0'..'\x7F').cycle().enumerate().take(ITERATIONS) {
+        let len = (i / 256) + 100;
+        let symbol = iter::repeat(ch).take(len).collect::<String>();
+
+        let sym_id = table.intern(symbol.clone()).unwrap();
+
+        assert!(table.is_interned(&symbol));
+        assert!(table.contains(sym_id));
+        assert_eq!(Some(symbol.as_str()), table.get(sym_id));
+        assert_eq!(sym_id, table.intern(symbol.clone()).unwrap());
+        assert!(table.is_interned(&symbol));
+        assert!(table.contains(sym_id));
+        assert_eq!(Some(symbol.as_str()), table.get(sym_id));
+    }
 }
