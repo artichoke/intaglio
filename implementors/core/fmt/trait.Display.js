c706670f19fd4d0745360d142882af4c54713ded
artichoke-ci <ci@artichokeruby.org>
2021-02-03 00:42:21 +0000

    deploy: 79500f621bc46c0e3462cbe4e8ccfb361b722edb

--- implementors/core/fmt/trait.Display.js
+++ implementors/core/fmt/trait.Display.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["intaglio"] = [{"text":"impl Display for SymbolOverflowError","synthetic":false,"types":[]}];
+implementors["intaglio"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"intaglio/struct.SymbolOverflowError.html\" title=\"struct intaglio::SymbolOverflowError\">SymbolOverflowError</a>","synthetic":false,"types":["intaglio::SymbolOverflowError"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["intaglio"] = [{"text":"impl Error for SymbolOverflowError","synthetic":false,"types":[]}];
+implementors["intaglio"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"intaglio/struct.SymbolOverflowError.html\" title=\"struct intaglio::SymbolOverflowError\">SymbolOverflowError</a>","synthetic":false,"types":["intaglio::SymbolOverflowError"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
